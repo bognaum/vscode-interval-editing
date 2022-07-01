a729a722bbdf9cb4293f5f8ceddf49854828ba27
Bognaum <bognaum@gmail.com>
2022-07-01 17:29:34 +0300

    Added settings.

--- src/commands/splitByLines.ts
+++ src/commands/splitByLines.ts
@@ -14,6 +14,7 @@ export default function divideByLines(
 		newSelections: vsc.Selection[] = [];
 	
 	tEditor.edit((edit) => {
+		const config = vsc.workspace.getConfiguration('divide-selection', doc.uri);
 		const 
 			positions: number[] = tEditor.selections.reduce((acc,v) => {
 				acc.push(v.start.line);
@@ -31,7 +32,7 @@ export default function divideByLines(
 				for (let i = firstLineNum; i <= lastLineNum; i ++) {
 					lineNumbers.push(i);
 				}
-				tEditor.selections = getSelectionsByLines(doc, lineNumbers);
+				tEditor.selections = getSelectionsByLines(doc, lineNumbers, config);
 			} else if (2 === commonCursorCount) {
 				const 
 					startP = doc.lineAt(firstLineNum).range.start,
@@ -53,7 +54,7 @@ export default function divideByLines(
 					lineNumbers.push(i);
 				}
 				
-				tEditor.selections = getSelectionsByLines(doc, lineNumbers);
+				tEditor.selections = getSelectionsByLines(doc, lineNumbers, config);
 			}
 		} else {
 			vsc.window.showWarningMessage("You need to select a fev lines or put a fev cursors to different lines.");
@@ -61,10 +62,41 @@ export default function divideByLines(
 	});
 }
 
-function getSelectionsByLines(doc: vsc.TextDocument, lineNumbers: number[])
+function getSelectionsByLines(
+	doc: vsc.TextDocument, 
+	lineNumbers: number[], 
+	config: vsc.WorkspaceConfiguration
+)
 : vsc.Selection[] {
 	return lineNumbers.map(v => {
-		const {start, end} = doc.lineAt(v).range;
-		return new vsc.Selection(end, end);
+		const 
+			text = doc.lineAt(v).text,
+			re = /(?<indentText>(?<indent>^\s*)(?<text>.*))\s*$/,
+			m = text.match(re) || {groups:{indent: "", text: "", indentText: ""}};
+		let start: number = 0, end: number = 0;
+
+		if (config.selectLineFrom === "start-of-line") {
+			start = 0;
+		} else if (config.selectLineFrom === "start-of-text") {
+			start = m.groups?.indent.length || 0;
+		} else if (config.selectLineFrom === "end-of-line") {
+			start = text.length;
+		} else if (config.selectLineFrom === "end-of-text") {
+			start = m.groups?.indentText.length || 0;
+		}
+
+		if (config.selectLineTo === "start-of-line") {
+			end = 0;
+		} else if (config.selectLineTo === "start-of-text") {
+			end = m.groups?.indent.length || 0;
+		} else if (config.selectLineTo === "end-of-line") {
+			end = text.length;
+		} else if (config.selectLineTo === "end-of-text") {
+			end = m.groups?.indentText.length || 0;
+		}
+
+		return new vsc.Selection(new vsc.Position(v, start), new vsc.Position(v, end));
+		/* const {start, end} = doc.lineAt(v).range;
+		return new vsc.Selection(end, end); */
 	});
 }

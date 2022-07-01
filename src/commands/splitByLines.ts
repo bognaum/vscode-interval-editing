0e69e01172352133862206905c93d425f506ee6a
Bognaum <bognaum@gmail.com>
2022-07-01 14:50:50 +0300

    It's working.

--- src/commands/splitByLines.ts
+++ src/commands/splitByLines.ts
@@ -1,6 +1,10 @@
 import * as vsc from "vscode";
 
-export default function divideByLines(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, ...args: any[]) {
+export default function divideByLines(
+	tEditor: vsc.TextEditor, 
+	edit: vsc.TextEditorEdit, 
+	...args: any[]
+) {
 	const 
 		doc  = tEditor.document,
 		opts = tEditor.options,
@@ -11,47 +15,61 @@ export default function divideByLines(tEditor: vsc.TextEditor, edit: vsc.TextEdi
 	
 	tEditor.edit((edit) => {
 		const 
-			positions: number[] = tEditor.selections.reduce((a,v) => {
-				a.push(v.start.line);
-				a.push(v.end.line);
-				return a;
+			positions: number[] = tEditor.selections.reduce((acc,v) => {
+				acc.push(v.start.line);
+				acc.push(v.end.line);
+				return acc;
 			}, new Array).sort(),
 			[firstLineNum, lastLineNum] = [positions[0], positions[positions.length - 1]],
-			commonLineCount = lastLineNum - firstLineNum + 1;
+			commonLineCount = lastLineNum - firstLineNum + 1,
+			commonCursorCount = tEditor.selections.length;
+
+		console.log(`positions >>`, positions);
 
 		console.log(`firstLineNum >>`, firstLineNum);
 		console.log(`lastLineNum >>`, lastLineNum);
 		console.log(`commonLineCount >>`, commonLineCount);
+		console.log(`commonCursorCount >>`, commonCursorCount);
 
 		if (1 < commonLineCount) {
-			const 
-				selCount = tEditor.selections.length,
-				breakPointCount = selCount === 1 ? commonLineCount : selCount,
-				calcLineCount = commonLineCount - 1,
-				calcBPCount = breakPointCount - 1,
-				numbers: number[] = [];
-			let step = Math.ceil(calcLineCount / calcBPCount);
-			console.log(`breakPointCount >>`, breakPointCount);
-			console.log(`step >>`, step);
-			step ++;
-			for (; step <= calcLineCount; step ++) {
-				if (calcLineCount % step) {continue;} else {break;}
-			}
-			console.log(`step >>`, step);
-			numbers.push(lastLineNum);
-
-			for (let i = lastLineNum; firstLineNum <= i; i -= step) {
-				numbers.push(i);
-			}
-
-			console.log(`numbers >>`, numbers);
-
-			tEditor.selections = numbers.map(v => {
+			
+			if (1 === commonCursorCount) {
+				const lineNumbers: number[] = [];
+				for (let i = firstLineNum; i <= lastLineNum; i ++) {
+					lineNumbers.push(i);
+				}
+				tEditor.selections = getSelectionsByLines(doc, lineNumbers);
+			} else if (2 === commonCursorCount) {
 				const 
-					{start, end} = doc.lineAt(v).range,
-					newSel = new vsc.Selection(start, end);
-				return newSel;
-			});
+					startP = doc.lineAt(firstLineNum).range.start,
+					endP = doc.lineAt(lastLineNum).range.end;
+				tEditor.selections = [new vsc.Selection(startP, endP)];
+				
+			} else {
+				const 
+					calcLineCount  : number = commonLineCount - 1,
+					calcCursorCount: number = commonCursorCount - 1,
+					lineNumbers  : number[] = [];
+				let step: number = Math.ceil(calcLineCount / (calcCursorCount - 1));
+					
+				console.log(`calcLineCount >>`, calcLineCount);
+				console.log(`calcCursorCount >>`, calcCursorCount);
+				console.log(`step >>`, step);
+				
+				for (;step <= calcLineCount; step ++) {
+					if (calcLineCount % step) {continue;} else {break;}
+				}
+				console.log(`step >>`, step);
+
+				for (let i = firstLineNum; i <= lastLineNum; i += step) {
+					lineNumbers.push(i);
+				}
+				lineNumbers.push(lastLineNum);
+				
+				console.log(`lineNumbers >>`, lineNumbers);
+				
+				tEditor.selections = getSelectionsByLines(doc, lineNumbers);
+			}
 			
 			// tEditor.selections = newSelections;
 		} else {
@@ -60,11 +78,10 @@ export default function divideByLines(tEditor: vsc.TextEditor, edit: vsc.TextEdi
 	});
 }
 
-function rangeToOffsets(doc: vsc.TextDocument, range: vsc.Range) {
-	return [range.start, range.end].map(doc.offsetAt);
-}
-
-function offsetsToRange(doc: vsc.TextDocument, offsets: [number, number]): vsc.Range {
-	const [a, b] = offsets.map(doc.positionAt);
-	return new vsc.Range(a, b);
+function getSelectionsByLines(doc: vsc.TextDocument, lineNumbers: number[])
+: vsc.Selection[] {
+	return lineNumbers.map(v => {
+		const {start, end} = doc.lineAt(v).range;
+		return new vsc.Selection(end, end);
+	});
 }
